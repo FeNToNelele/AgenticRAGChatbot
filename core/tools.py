c35cbdaf9ec187fffaa18954f2d21d5f31accf77
FeNToNelele <sarosigab@gmail.com>
2025-09-22 19:28:59 +0200

    Added LangGraph with nodes, removed Groq from project, refactored workflow.

--- core/tools.py
+++ core/tools.py
@@ -2,7 +2,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_history_aware_retriever
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -12,7 +11,20 @@ from langchain.storage import LocalFileStore
 from langchain.embeddings import CacheBackedEmbeddings
 import torch, os
 
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain.llms import HuggingFacePipeline
+
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, List, Tuple
 from datasets import load_dataset
+
+class RAGState(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
 
 VECTOR_DB_DIR = "chroma_db"
 EMBEDDING_CACHED_DIR = "embedding_cache"
@@ -24,19 +36,13 @@ print(f"Using device: {device}")
 
 GROQ_API_KEY = os.environ.get('AgenticRAGChatbot_APIKEY')
 
-llm = ChatGroq(
-    temperature=0,
-    model_name="llama-3.1-8b-instant",
-    api_key=GROQ_API_KEY
-)
-
-underlying_embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={'device': device})
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={'device': device})
 store = LocalFileStore(EMBEDDING_CACHED_DIR)
 
 cached_embedding = CacheBackedEmbeddings.from_bytes_store(
-    underlying_embeddings=underlying_embeddings,
+    underlying_embeddings=embeddings,
     document_embedding_cache=store,
-    namespace=underlying_embeddings.model_name
+    namespace=embeddings.model_name
 )
 
 if not os.path.exists(VECTOR_DB_DIR):
@@ -69,21 +75,70 @@ print("Getting retriever...")
 retriever = db.as_retriever()
 print("Done.")
 
-contextualizer_prompt = ChatPromptTemplate.from_messages([
-    ("system", "Given a chat history and the latest user question which might reference context in the chat history, formulate a standalone question which can be understood without the chat history."),
-    ("human", "{input}"),
-])
-history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualizer_prompt)
+selected_model = "meta-llama/Llama-3.1-8B-Instruct"
+
+print(f"Loading model")
+tokenizer = AutoTokenizer.from_pretrained(selected_model)
+model = AutoModelForCausalLM.from_pretrained(
+    selected_model,
+    device_map="auto",
+    load_in_4bit=True,
+    torch_dtype=torch.float16,
+)
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=512,
+    temperature=0.2,
+)
+
+llm = HuggingFacePipeline(pipeline=pipe)
 
 qa_prompt = ChatPromptTemplate.from_messages([
-    ("system", "Answer the user's question based on the provided context only. If you don't know the answer, just say you don't know, don't try to make up an answer. \n\n{context}"),
+    ("system", "Answer the user's question based on the provided context only.\n\n{context}"),
     ("human", "{input}"),
 ])
-question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
-def rag_chain_executor(state):
-    """Executes the RAG chain to retrieve context and generate a response."""
-    question = state["messages"][-1].content
-    retrieved_docs = history_aware_retriever.invoke({"input": question, "chat_history": state["messages"][:-1]})
-    response = question_answer_chain.invoke({"input": question, "context": retrieved_docs, "chat_history": state["messages"][:-1]})
-    return {"messages": [("ai", response.content)]}
+qa_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+def retrieve_node(state: RAGState) -> RAGState:
+    print("[Retriever] Searching for documents...")
+    docs = retriever.invoke(state["question"])
+    state["context"] = docs
+    return state
+
+def qa_node(state: RAGState) -> RAGState:
+    print("[QA] Generating answer...")
+    response = qa_chain.invoke({
+    "input": state["question"],
+    "context": state.get("context", [])
+    })
+    state["answer"] = response.content
+    return state
+
+# Only for demonstration purposes
+# If question is shorter than 5 words, use QA, else retrieve info.
+def controller_node(state: RAGState) -> str:
+    if len(state["question"].split()) < 5:
+        print("[Controller] No need for retrieval.")
+        return "qa"
+    else:
+        print("[Controller] Use retrieval.")
+        return "retrieve"
+
+graph = StateGraph(RAGState)
+graph.add_node("retrieve", retrieve_node)
+graph.add_node("qa", qa_node)
+
+graph.set_entry_point(controller_node)
+graph.add_edge("retrieve", "qa")
+graph.add_edge("qa", END)
+
+rag_chain = graph.compile()
+
+def rag_chain_executor(question: str):
+    state = {"question": question, "context": [], "answer": ""}
+    final_state = rag_chain.invoke(state)
+    return final_state["answer"]
